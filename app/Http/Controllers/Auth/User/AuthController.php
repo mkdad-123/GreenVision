51fb4978f78e1730a01bf8851045f09cb530126f
mkdad-123 <ma0109424@gmail.com>
2025-09-05 17:34:13 -0800

    Fix controller namespace issue

--- app/Http/Controllers/Auth/User/AuthController.php
+++ app/Http/Controllers/Auth/User/AuthController.php
@@ -9,13 +9,10 @@ use App\Http\Requests\RegisterRequest;
 use App\Models\Admin;
 use App\Services\User\Auth\LoginService;
 use App\Services\User\Auth\RegisterService;
-use App\Services\EmailVerificationService;
 use App\Traits\ResetPasswordTrait;
 use GuzzleHttp\Exception\ClientException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Laravel\Socialite\Facades\Socialite;
-use Laravel\Socialite\Contracts\User as SocialiteUser;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
@@ -98,54 +95,4 @@ class AuthController extends Controller
         return response()->json(['user' => $user], 200);
     }
 
-    // public function verify(Request $request)
-    // {
-    //     return (new EmailVerificationService())->verify($request);
-    // }
-
-    // public function redirectToAuth()
-    // {
-    //     return response()->json([
-    //         'url' => Socialite::driver('google')
-    //             ->stateless()
-    //             ->redirect()
-    //             ->getTargetUrl(),
-    //     ]);
-    // }
-
-    // public function handleAuthCallback()
-    // {
-    //     try {
-    //         /** @var SocialiteUser $socialiteUser */
-    //         $socialiteUser = Socialite::driver('google')->stateless()->user();
-    //     } catch (ClientException $e) {
-    //         return response()->json(['error' => 'Invalid credentials provided.'], 422);
-    //     }
-
-    //     /** @var Admin $user */
-    //     $user = Admin::query()
-    //         ->firstOrCreate(
-    //             [
-    //                 'email' => $socialiteUser->getEmail(),
-    //             ],
-    //             [
-    //                 'email_verified_at' => now(),
-    //                 'name' => $socialiteUser->getName(),
-    //                 'google_id' => $socialiteUser->getId(),
-    //                 'avatar' => $socialiteUser->getAvatar(),
-    //                 'password' => bcrypt($socialiteUser->getName().$socialiteUser->getId())
-    //             ]
-    //         );
-
-    //     $token = Auth::guard('admin')->attempt([
-    //         'email' => $user->email,
-    //         'password'=> $socialiteUser->getName().$socialiteUser->getId(),
-    //     ]);
-
-    //     return response()->json([
-    //         'user' => $user,
-    //         'access_token' => $token,
-    //         'token_type' => 'Bearer',
-    //     ]);
-    // }
 }
